e5315c51fcc98736f4b8d14f06de24a362110aad
Manuela23242 <152196997+Manuela23242@users.noreply.github.com>
2024-07-18 16:42:58 +0200

    Add files via upload

---
+++ songs.js
@@ -0,0 +1,71 @@
+document.getElementById("image").addEventListener("click", function () {
+    document.getElementById("bp").play();
+
+document.getElementById("image").addEventListener("click", function () {
+    document.getElementById("bp").pause();
+})
+});
+
+document.getElementById("imagee").addEventListener("click", function () {
+    document.getElementById("meet").play();
+
+document.getElementById("imagee").addEventListener("click", function () {
+    document.getElementById("meet").pause();
+})
+});
+
+document.getElementById("3").addEventListener("click", function () {
+    document.getElementById("rave").play();
+
+document.getElementById("3").addEventListener("click", function () {
+    document.getElementById("rave").pause();
+})
+});
+
+document.getElementById("4").addEventListener("click", function () {
+    document.getElementById("Phonk").play();
+
+document.getElementById("4").addEventListener("click", function () {
+    document.getElementById("Phonk").pause();
+})
+});
+
+document.getElementById("5").addEventListener("click", function () {
+    document.getElementById("Pushing").play();
+
+document.getElementById("5").addEventListener("click", function () {
+    document.getElementById("Pushing").pause();
+})
+});
+
+document.getElementById("6").addEventListener("click", function () {
+    document.getElementById("favhela").play();
+
+document.getElementById("6").addEventListener("click", function () {
+    document.getElementById("favhela").pause();
+})
+});
+
+document.getElementById("7").addEventListener("click", function () {
+    document.getElementById("mucky").play();
+
+document.getElementById("7").addEventListener("click", function () {
+    document.getElementById("mucky").pause();
+})
+});
+
+document.getElementById("8").addEventListener("click", function () {
+    document.getElementById("techno").play();
+
+document.getElementById("8").addEventListener("click", function () {
+    document.getElementById("techno").pause();
+})
+});
+
+document.getElementById("9").addEventListener("click", function () {
+    document.getElementById("Comet").play();
+
+document.getElementById("9").addEventListener("click", function () {
+    document.getElementById("Comet").pause();
+})
+});
